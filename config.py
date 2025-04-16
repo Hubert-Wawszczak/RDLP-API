7f7a95ee84db79a6439f358cfae487c576847edf
wismich <michal.t.wisniewski@gmail.com>
2025-04-16 21:31:17 +0200

    wczytywanie oraz walidowanie danych z pliku .env z opjcą zmiany środowiska

--- config.py
+++ config.py
@@ -1,18 +1,41 @@
-import os
-from pydantic_settings import BaseSettings
+import sys
+
 from dotenv import load_dotenv
+from path import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
-class Config:
-    def __init__(self, env_file: str = "config.env"):
-        load_dotenv(env_file)
-
+# TODO: proper logging instead of print
 
 class Settings(BaseSettings):
-    Config()
-    database_url: str = os.getenv("DB_HOST")
-    database_port: str = os.getenv("DB_PORT")
-    database_name: str = os.getenv("DB_NAME")
-    database_username: str = os.getenv("DB_USERNAME")
-    database_password: str = os.getenv("DB_PASSWORD")
+
+    db_host: str = Field(...)
+    db_port: str = Field(...)
+    db_name: str = Field(...)
+    db_username: str = Field(...)
+    db_password: str = Field(...)
 
 
+    def __init__(self, env: str = 'dev', **kwargs):
+        try:
+            env_file: Path = Path(f"config.{env}.env")
+            if not env_file.exists():
+                raise FileNotFoundError("Environment file not found")
+            load_dotenv(env_file)
+            super().__init__(**kwargs)
+        except FileNotFoundError:
+            print("file not found")
+            return
+        except OSError:
+            print("could not open/read file")
+            return
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit()
+
+    class Config:
+        model_config = SettingsConfigDict(env_file_encoding='utf-8')
+
+
+asdf = Settings(env="prod")
+print("settings", asdf.model_dump())
