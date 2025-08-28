d37429de24e0e9f1bc9140492f7e5feee3049867
wismich <michal.t.wisniewski@gmail.com>
2025-08-28 14:19:40 +0200

    expanded capability to fetch data from API by first getting a sample batch from which a total number of records is extracted and based on which further asynchronous fetch tasks are created

--- services/api_client.py
+++ services/api_client.py
@@ -1,10 +1,13 @@
 
 import aiohttp
+import aiofiles
 import asyncio
-import json
 
+from pathlib import Path
+from datetime import datetime
 from typing import Literal, get_args
 from utils.logger.logger import AsyncLogger
+
 
 logger = AsyncLogger()
 
@@ -12,9 +15,6 @@ class APIClient:
     __API = "https://ogcapi.bdl.lasy.gov.pl/collections/"
     __ENDPOINTS = Literal[
         'all',
-        'rdlp',
-        'nadlesnictwa',
-        'lesnictwa',
         'RDLP_Bialystok_wydzielenia',
         'RDLP_Katowice_wydzielenia',
         'RDLP_Krakow_wydzielenia',
@@ -34,9 +34,6 @@ class APIClient:
         'RDLP_Warszawa_wydzielenia'
     ]
     __ENDPOINTS_LIST = [
-        'rdlp',
-        'nadlesnictwa',
-        'lesnictwa',
         'RDLP_Bialystok_wydzielenia',
         'RDLP_Katowice_wydzielenia',
         'RDLP_Krakow_wydzielenia',
@@ -56,71 +53,133 @@ class APIClient:
         'RDLP_Warszawa_wydzielenia'
     ]
 
-    async def __fetch_data_page(self, session: aiohttp.ClientSession, url: str):
-        logger.log("INFO", f"Fetching data from {url}")
+    @staticmethod
+    async def __get_item_total(session: aiohttp.ClientSession, url: str):
+        """
+        Fetches the total number of items available at the given API endpoint.
+
+        Args:
+            session (aiohttp.ClientSession): The active HTTP session.
+            url (str): The URL to fetch the item count from.
+
+        Returns:
+            int: The total number of items, or 0 if the request fails.
+        """
+        logger.log("INFO", f"Fetching item total from {url}")
         async with session.get(url) as response:
             if response.status == 200:
-                return await response.json()
+                data = await response.json()
+                return data.get('numberMatched', 0)
+            else:
+                logger.log("ERROR", f"Failed to fetch item total from {url}: {response.status}")
+                return 0
+
+    @staticmethod
+    async def __fetch_data_page(session: aiohttp.ClientSession, url: str, offset: int = 0, save_dir: Path = Path('G:\\PilarzOPS\\RDLP-API\\temp_data')):
+        """
+        Fetches a single page of data from the API and saves it to a file.
+
+        Args:
+            session (aiohttp.ClientSession): The active HTTP session.
+            url (str): The URL to fetch data from.
+            offset (int, optional): The offset for pagination. Defaults to 0.
+            save_dir (Path, optional): Directory to save the fetched data. Defaults to 'G:\\PilarzOPS\\RDLP-API\\temp_data'.
+
+        Returns:
+            bool: True if data was fetched and saved successfully, False otherwise.
+        """
+        save_dir.mkdir(exist_ok=True)
+
+        filename = url.split('/')[-2] + f"_{offset}_" + str(int(datetime.now().timestamp())) + ".json"
+        async with session.get(url) as response:
+            if response.status == 200:
+                text = await response.text(encoding='utf-8')
+                async with aiofiles.open(save_dir / filename, "w", encoding='utf-8') as f:
+                    # async for chunk in response.content.iter_chunked(1024):
+                    await f.write(text)
+                return True
             else:
                 logger.log("ERROR", f"Failed to fetch data from {url}: {response.status}")
-                return {}
+                return False
 
 
     @logger.log_time_exec
     async def __fetch_everything(self, session: aiohttp.ClientSession, endpoint: str, limit: int = 1000):
-        data = []
-        # fetch first page of data and extract total number of items from response json
+        """
+        Fetches all data for a given endpoint, handling pagination and saving each page.
+
+        Args:
+            session (aiohttp.ClientSession): The active HTTP session.
+            endpoint (str): The API endpoint to fetch data from.
+            limit (int, optional): Number of items per page. Defaults to 1000.
+
+        Returns:
+            bool: True if all data was fetched successfully.
+        """
+
+        # fetch first page of data
         request_url = f"{self.__API}{endpoint}/items"
-        initial = await self.__fetch_data_page(session, request_url + f"?f=json&limit={limit}&offset=0")
-        data.append(initial)
-        total = initial.get('numberMatched', 0)
+        await self.__fetch_data_page(session, request_url + f"?f=json&limit={limit}&offset=0")
+
+        # get total number of items for given endpoint
+        total = await self.__get_item_total(session, request_url + '?f=json&limit=1&skipGeometry=true')
         logger.log("INFO", f"Total items found for {endpoint}: {total}")
         # split fetch jobs into batches if there is more than set limit
         if total > limit:
+            logger.log("INFO", f"Fetching data in batches of {limit} for {endpoint}. Total items: {total}")
             tasks = [
-                self.__fetch_data_page(session, request_url + f"?f=json&limit={limit}&offset={offset}")
+                self.__fetch_data_page(session, request_url + f"?f=json&limit={limit}&offset={offset}", offset)
                 for offset in range(limit, total + limit, limit)
             ]
-
-            results = await asyncio.gather(*tasks)
-            data.extend(results)
+            await asyncio.gather(*tasks)
             logger.log("INFO", f"Fetched {total} data entries from {endpoint}")
-        return data
+        return True
 
-    async def fetch_data(self, endpoints: list[__ENDPOINTS]):
-        logger.log("INFO", f"Fetching data for endpoints: {endpoints}")
-        print(len(endpoints))
+    async def fetch_data(self, endpoints: list[__ENDPOINTS], limit: int = 1000):
+        """
+        Fetches data for the specified endpoints, supporting single, multiple, or all endpoints.
+
+        Args:
+            endpoints (list[__ENDPOINTS]): List of endpoint names to fetch data from.
+            limit (int, optional): Number of items per page. Defaults to 1000.
+
+        Returns:
+            bool: True if data was fetched successfully, False otherwise.
+        """
+
         options = get_args(self.__ENDPOINTS)
         for item in endpoints:
             assert item in options, logger.log("ERROR", f"Invalid endpoint: {endpoints}. Must be one of {self.__ENDPOINTS}")
-        limit = 1000
-        data = []
         try:
             async with aiohttp.ClientSession() as session:
                 if "all" in endpoints:
                     logger.log("INFO", "Fetching data for 'all' endpoints.")
-                    for endpoint in self.__ENDPOINTS_LIST:
-                        data.extend(await self.__fetch_everything(session, endpoint, limit))
-                    return data
+                    tasks = [
+                        self.__fetch_everything(session, e, limit)
+                        for e in self.__ENDPOINTS_LIST
+                    ]
+                    await asyncio.gather(*tasks)
+                    return True
                 elif len(endpoints) == 1:
-                    logger.log("INFO", f"Fetching data for single endpoint: {endpoints[0]}.")
-                    data = await self.__fetch_everything(session, endpoints[0], limit)
-                    return data
+                    logger.log("INFO", f"Fetching data for single specified endpoint: {endpoints[0]}.")
+                    await self.__fetch_everything(session, endpoints[0], limit)
+                    return True
                 else:
-                    logger.log("INFO", f"Fetching data for multiple endpoints: {endpoints}.")
-                    for endpoint in endpoints:
-                        data.extend(await self.__fetch_everything(session, endpoint, limit))
-                    return data
+                    logger.log("INFO", f"Fetching data for multiple specified endpoints: {endpoints}.")
+                    tasks = [
+                        self.__fetch_everything(session, e, limit)
+                        for e in endpoints
+                    ]
+                    await asyncio.gather(*tasks)
+                    return True
         except Exception as e:
             logger.log("ERROR", f"Failed to fetch data for 'all' endpoints: {str(e)}")
-            return []
+            return False
 
 
 if __name__ == "__main__":
     async def main():
         api_client = APIClient()
-        test_data = await api_client.fetch_data(['RDLP_Bialystok_wydzielenia', 'lesnictwa'])
-        with open("test_data.json", "w") as f:
-            f.write(json.dumps(test_data))
+        await api_client.fetch_data(['RDLP_Bialystok_wydzielenia', 'RDLP_Lublin_wydzielenia'])
 
     asyncio.run(main())
