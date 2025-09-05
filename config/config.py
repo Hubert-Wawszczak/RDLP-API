e0457a3c54412a66f49ecdfaba9112d8a837c6d8
wismich <michal.t.wisniewski@gmail.com>
2025-09-05 10:59:11 +0200

    changed log messages to use AsyncLogger instead of print statements

--- config/config.py
+++ config/config.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 from path import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
+from utils.logger.logger import AsyncLogger
 
-# TODO: proper logging instead of print
 
 class Settings(BaseSettings):
 
@@ -15,27 +15,27 @@ class Settings(BaseSettings):
     db_username: str = Field(...)
     db_password: str = Field(...)
 
-
+logger = AsyncLogger()
     def __init__(self, env: str = 'dev', **kwargs):
         try:
-            env_file: Path = Path(f"config.{env}.env")
+            env_file: Path = Path(f"G:\\PilarzOPS\\RDLP-API\\config.{env}.env")
             if not env_file.exists():
                 raise FileNotFoundError("Environment file not found")
             load_dotenv(env_file)
             super().__init__(**kwargs)
         except FileNotFoundError:
-            print("file not found")
+            logger.log("ERROR", f"Environment file not found: {env_file}")
             return
         except OSError:
-            print("could not open/read file")
+            logger.log("ERROR", f"Environment file not found: {env_file}")
             return
         except Exception as e:
-            print(f"An error occurred: {e}")
+            logger.log("ERROR", f"Unexpected error: {e}")
             sys.exit()
 
     class Config:
         model_config = SettingsConfigDict(env_file_encoding='utf-8')
 
-
-asdf = Settings(env="prod")
-print("settings", asdf.model_dump())
+if __name__ == "__main__":
+    asdf = Settings(env="prod")
+    print("settings", asdf.model_dump())
